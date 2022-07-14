8406a0fba94e42a033617c1358d9415b9097bb6a
HarrisonNjuguna <machariaharry12@gmail.com>
2022-07-14 19:20:21 +0300

    Dart/Flutter

---
+++ bin/test_project.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+void main() {
+  print(sin(45 * pi / 180));
+  print(1 / sqrt(2));
+//0.7071067811865475
+
+  print(10.isOdd);
+  print(3.14159.round());
+
+  var myNumber;
+  myNumber = 3.14159;
+
+  var myVariably;
+  myVariably = 10;
+  myVariably = 'ten';
+  myVariably = 3.14159;
+
+  //mini exercise
+  const int myAge = 24;
+  print(myAge);
+
+  double averageAge;
+  averageAge = (24 + 22) / 2;
+  print(averageAge);
+
+  const testNumber = 150;
+  const evenOdd = testNumber % 2;
+  print(evenOdd);
+}
+
+const myConstant = 10;
---
+++ test/test_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_project/test_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
